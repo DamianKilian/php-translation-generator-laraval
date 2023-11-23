92c96d09a75b3d038ee6f965506f8bb108aa1663
Damian Kilian <kilianpoczta@gmail.com>
2023-11-23 11:41:24 +0100

    install bootstrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,6 @@
 
 <head>
     <title>php-translation-manager-laravel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ route('css') }}">
 </head>
 
@@ -12,9 +10,6 @@
     <div id="app">
         <php-translation-manager :trans-files-contents='@json($transFilesContents)'></php-translation-manager>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
     <script src="{{ route('js') }}"></script>
 </body>
 
