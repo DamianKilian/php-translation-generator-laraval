dd879ac132684d6b43548c34755b65350b760203
Damian Kilian <kilianpoczta@gmail.com>
2024-02-05 08:22:05 +0100

    fix

--- tests/Unit/TransTest.php
+++ tests/Unit/TransTest.php
@@ -10,17 +10,21 @@ class TransTest extends TestCase
 
     public function test_wrapElementsInArray()
     {
-        $phpTranslationManagerService = new PhpTranslationManagerService('tt');
+        $phpTranslationManagerService = new PhpTranslationManagerService('');
         $testArr = [
             'k1' => 'v1',
             'k2' => 'v2',
             'k3' => 'v3',
         ];
-        $testArrWrapped = $phpTranslationManagerService->wrapElementsInArray($testArr);
-        $testArrUnwrapped = [];
-        foreach ($testArrWrapped as $value) {
-            $testArrUnwrapped[$value['key']] = $value['val'];
-        }
-        $this->assertTrue($testArrUnwrapped === $testArr);
+        $testArrWrapped = [
+            ['key' => 'k1', 'val' => 'v1'],
+            ['key' => 'k2', 'val' => 'v2'],
+            ['key' => 'k3', 'val' => 'v3'],
+        ];
+
+        $testArrWrappedService = $phpTranslationManagerService->wrapElementsInArray($testArr);
+
+        $this->assertTrue($testArrWrapped === $testArrWrappedService);
     }
+
 }
