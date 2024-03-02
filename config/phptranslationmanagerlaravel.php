1db44d262a50af0a2348d0769778cbf080a8c70f
Damian Kilian <kilianpoczta@gmail.com>
2024-03-02 12:24:37 +0100

    fix

--- config/phptranslationmanagerlaravel.php
+++ config/phptranslationmanagerlaravel.php
@@ -8,19 +8,21 @@ $trimRegex = <<<'END'
 /^__\(\s*'\s*|^__\(\s*"\s*|\s*'\s*\)$|\s*"\s*\)$/
 END;
 
+$ferp = function_exists('resource_path');
+
 return [
-    'lang_path' => 'resources/lang',
+    'lang_path' => $ferp ? resource_path() . '/lang' : null,
     'search_locations' => [
         '/resources/views' => [
             'regex' => $regex,
             'trimRegex' => $trimRegex,
-            'path' => 'resources/views',
+            'path' => $ferp ? resource_path() . '/views' : null,
             'file_extensions' => ['blade.php'],
         ],
         '/resources/js' => [
             'regex' => $regex,
             'trimRegex' => $trimRegex,
-            'path' => 'resources/js',
+            'path' => $ferp ? resource_path() . '/js' : null,
             'file_extensions' => ['vue'],
         ],
     ]
