f52e233654f9c7fd6ccb11a929e95fd0286e790d
Damian Kilian <kilianpoczta@gmail.com>
2024-03-13 13:22:26 +0100

    ActionsTest

--- tests/Feature/ActionsTest.php
+++ tests/Feature/ActionsTest.php
@@ -3,6 +3,8 @@
 namespace PhpTranslationManagerLaravel\Tests\Feature;
 
 use PhpTranslationManagerLaravel\Tests\TestCase;
+use PhpTranslationManagerLaravel\Service\PhpTranslationManagerService;
+use Mockery\MockInterface;
 
 class ActionsTest extends TestCase
 {
@@ -10,6 +12,47 @@ class ActionsTest extends TestCase
     {
         $response = $this->get('phptranslationmanager');
         $response->assertStatus(200);
+    }
+
+    public function test_getTransFilesContentsData(): void
+    {
+        $this->mock(PhpTranslationManagerService::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getTransFilesContents')->once();
+        });
+
+        $response = $this->postJson('phptranslationmanager/get-trans-files-contents-data');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_saveTransFiles(): void
+    {
+        $this->mock(PhpTranslationManagerService::class, function (MockInterface $mock) {
+            $mock->shouldReceive('saveTransFiles')->once();
+        });
+
+        $response = $this->postJson('phptranslationmanager/save-trans-files');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+            ]);
+    }
+
+    public function test_throw_exception_saveTransFiles(): void
+    {
+        $this->mock(PhpTranslationManagerService::class, function (MockInterface $mock) {
+            $mock->shouldReceive('saveTransFiles')->once()->andThrowExceptions([new \Exception()]);
+        });
+
+        $response = $this->postJson('phptranslationmanager/save-trans-files');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => false,
+            ]);
     }
 
     protected function getPackageProviders($app)
