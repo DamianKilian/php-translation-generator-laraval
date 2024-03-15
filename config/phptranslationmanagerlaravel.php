f751b904932fe96a467581aea3df8febfcb249cc
Damian Kilian <kilianpoczta@gmail.com>
2024-03-15 09:43:45 +0100

    descriptions in config

--- config/phptranslationmanagerlaravel.php
+++ config/phptranslationmanagerlaravel.php
@@ -1,9 +1,19 @@
 <?php
 
+/*
+|
+| regex for finding translations e.g. __('trans1'), __("trans1")
+|
+*/
 $regex = <<<'END'
 /__\(\s*'\s*([^']*(\\')*[^']*)*\s*'\s*\)|__\(\s*"\s*([^"]*(\\")*[^"]*)*\s*"\s*\)/
 END;
 
+/*
+|
+| regex for triminng e.g. __('trans1') to trans1
+|
+*/
 $trimRegex = <<<'END'
 /^__\(\s*'\s*|^__\(\s*"\s*|\s*'\s*\)$|\s*"\s*\)$/
 END;
@@ -11,6 +21,12 @@ END;
 return [
     'translate_from' => 'en',
     'translation_api' => 'google',
+
+    /*
+    |
+    | Location of language files (currently .json are supported only)
+    |
+    */
     'lang_path' => resource_path() . '/lang',
     'search_locations' => [
         '/resources/views' => [
